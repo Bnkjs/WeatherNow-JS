f33bdb4cd4148579d08e2d27e59194c5f47ed121
Boris <seikenfr@yahoo.fr>
2021-09-01 17:59:15 +0200

    first commit

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+export function Header(){
+  return( <>
+      <header>
+        <h1>WeatherNow</h1>
+        <p>La méteo? D'accord.. <br />Mais de quelle ville? </p>
+      </header>
+    </>
+  )
+}
