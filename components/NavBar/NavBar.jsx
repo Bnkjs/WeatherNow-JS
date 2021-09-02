b827f3ebbce9d568d7d1abcf3461818afadba9eb
Boris <seikenfr@yahoo.fr>
2021-09-02 16:32:35 +0200

    updated:NavBar compoenent + others link files

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import svgBrand from '../../assets/svg/cloudy.svg'
+
 export const Navbar = () => {
   return(<>
     <nav id='nav'>
       <ul>
-        <li>Logo</li>
+        <li className='nav-brand'>
+          <Image
+          src={svgBrand}
+          />
+        </li>
+        <li>
+          <p>WN</p>
+          </li>
+        <li className='nav-brand'>
+          <Image
+          src={svgBrand}
+          />
+        </li>
       </ul>
     </nav>
   </>)
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Header } from '../components/Header/Header'
-import { SearchBar } from '../components/SearchBar/SearchBar' 
 import { Navbar } from '../components/NavBar/NavBar'
+
 export default function Home() {
   return (<div id="container-app">
       <Navbar/>
